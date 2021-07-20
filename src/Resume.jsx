15a39192d8599b205afaaa3b416ae9c6e0792456
Brigham Andersen <brighamband@gmail.com>
2021-07-20 16:38:00 -0600

    added screenshot

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,10 +1,14 @@
+import { Container } from "@material-ui/core";
 import React from "react";
 import resumePng from "./assets/resume.png";
 
 const Resume = () => (
   <div className="content resume-page">
-    <h1>Resume</h1>
-    <img src={resumePng} alt="Resume" className="resume" />
+     <h1>Resume</h1>
+  {/* <Container maxWidth="sm"> */}
+     <img src={resumePng} alt="Resume" className="resume" />
+
+  {/* </Container> */}
   </div>
 );
 
