ec0728c6936938a2ccbbb4f8ad74606b45bffb85
Brigham Andersen <brighamband@gmail.com>
2021-07-20 17:54:28 -0600

    Added icons to contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,27 +1,41 @@
+import { IconButton, Tooltip } from "@material-ui/core";
 import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import PhoneIcon from "@material-ui/icons/Phone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
 
 const Contact = () => (
   <div className="content contact-page">
     <h1>Contact Me</h1>
-    <ul>
-      <li>
-        <em>Email:</em>
-        <a href="mailto:brighamband@gmail.com">brighamband@gmail.com</a>
-      </li>
-      <li>
-        <em>LinkedIn:</em>
-        <a href="https://www.linkedin.com/in/brighamband">
-          linkedin.com/in/BrighamBAnd
-        </a>
-      </li>
-      <li>
-        <em>Phone:</em> <a href="tel:3854998277">385-499-8277</a>
-      </li>
-      <li>
-        <em>GitHub: </em>
-        <a href="https://github.com/brighamband/">brighamband's GitHub</a>
-      </li>
-    </ul>
+    <Tooltip title="Check out my GitHub" arrow>
+      <IconButton
+        href="https://github.com/brighamband/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GitHubIcon fontSize="large" className="github-contact-pg" />
+      </IconButton>
+    </Tooltip>
+    <Tooltip title="Connect with me on LinkedIn" arrow>
+      <IconButton
+        href="https://www.linkedin.com/in/brighamband"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <LinkedInIcon fontSize="large" />
+      </IconButton>
+    </Tooltip>
+    <Tooltip title="Email brighamband@gmail.com" arrow>
+      <IconButton href="mailto:brighamband@gmail.com">
+        <MailOutlineIcon fontSize="large" />
+      </IconButton>
+    </Tooltip>
+    <Tooltip title="Text / Call 385-499-8277" arrow>
+      <IconButton href="tel:3854998277">
+        <PhoneIcon fontSize="large" />
+      </IconButton>
+    </Tooltip>
   </div>
 );
 
--- src/Sites.jsx
+++ src/Sites.jsx
@@ -1,40 +1,50 @@
 import React from "react";
-import LaunchSharpIcon from '@material-ui/icons/LaunchSharp';
-import GitHubIcon from '@material-ui/icons/GitHub';
+import LaunchSharpIcon from "@material-ui/icons/LaunchSharp";
+import GitHubIcon from "@material-ui/icons/GitHub";
 import { IconButton, Tooltip } from "@material-ui/core";
 import sites from "./assets/sites";
 
 const TooltipTitle = ({ url, isSrcCode = false }) => (
   <>
-    {isSrcCode ? 'Source Code:' : 'Site:'}
-    <br/>
+    {isSrcCode ? "Source Code:" : "Site:"}
+    <br />
     <code className="url-code">{url}</code>
   </>
-)
+);
 
 const Sites = () => (
   <div className="content sites-page">
     <h1>Sites</h1>
     <ul>
-    {sites.map(site => (
+      {sites.map((site) => (
         <div key={site.url} className="card">
-            <h3>{site.name}</h3>
-            
-              <Tooltip title={<TooltipTitle url={site.url} />} arrow>
-                <IconButton href={site.url} target="_blank" rel="noopener noreferrer">
-                  <LaunchSharpIcon />
-                </IconButton>
-              </Tooltip>
-              {site.srcCode && (
-                <Tooltip title={<TooltipTitle url={site.srcCode} isSrcCode />} arrow>
-                  <IconButton href={site.url} target="_blank" rel="noopener noreferrer">
-                    <GitHubIcon />
-                  </IconButton>
-                </Tooltip>
-              )}
-              
+          <h3>{site.name}</h3>
+
+          <Tooltip title={<TooltipTitle url={site.url} />} arrow>
+            <IconButton
+              href={site.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LaunchSharpIcon />
+            </IconButton>
+          </Tooltip>
+          {site.srcCode && (
+            <Tooltip
+              title={<TooltipTitle url={site.srcCode} isSrcCode />}
+              arrow
+            >
+              <IconButton
+                href={site.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHubIcon />
+              </IconButton>
+            </Tooltip>
+          )}
         </div>
-    ))}
+      ))}
     </ul>
   </div>
 );
