48a4446235345950a8e43e5e3e8d3440657df111
Brigham Andersen <brighamband@gmail.com>
2021-08-03 10:45:51 -0600

    Fixed profile image sizing

--- src/Home.jsx
+++ src/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => (
           you!
         </p>
       </Grid>
-      <Grid item xs={12} md={4}>
+      <Grid item xs={8} md={4}>
         <img className="profile" src={profile} alt="Me" />
       </Grid>
     </Grid>
