e4b155db80734b727fb288909c806d6229e7cadd
Brigham Andersen <brighamband@gmail.com>
2021-08-02 22:45:54 -0600

    Finished resume

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,4 +1,4 @@
-import { Container, makeStyles, Typography } from "@material-ui/core";
+import { Container, makeStyles } from "@material-ui/core";
 import React from "react";
 import resume from "./assets/resume.png";
 
