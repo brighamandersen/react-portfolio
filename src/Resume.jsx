11915bb8a3936bbf146c523a849eb5f31af5ec36
Brigham Andersen <brighamband@gmail.com>
2021-08-02 23:00:19 -0600

    Fixed resume

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -2,22 +2,11 @@ import { Container, makeStyles } from "@material-ui/core";
 import React from "react";
 import resume from "./assets/resume.png";
 
-const useStyles = makeStyles(() => ({
-  resume: {
-    boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)",
-    width: "100%",
-  },
-}));
-
-const Resume = () => {
-  const classes = useStyles();
-
-  return (
-    <Container maxWidth="md" className="content">
-      <h1>Resume</h1>
-      <img src={resume} alt="Resume" className={classes.resume} />
-    </Container>
-  );
-};
+const Resume = () => (
+  <Container maxWidth="md" className="content">
+    <h1>Resume</h1>
+    <img src={resume} alt="Resume" className="resume" />
+  </Container>
+);
 
 export default Resume;
