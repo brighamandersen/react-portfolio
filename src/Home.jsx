9c88c82580efac002889b4ec516f02d07d30f3fb
Brigham Andersen <brighamband@gmail.com>
2021-08-02 22:26:29 -0600

    Home done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,27 +1,33 @@
+import { Container, Grid, Typography } from "@material-ui/core";
 import React from "react";
-import myBanner from "./assets/my-banner.png";
-import profilePic from "./assets/profile-pic.jpg";
+import profile from "./assets/profile-square.jpg";
 
 const Home = () => (
-  <div className="content home-page">
-    <h1>Home</h1>
-    <p>
-      Hi, my name is <strong>Brigham Andersen</strong>. I'm a student at Brigham
-      Young University getting a Bachelor's of Science in Computer Science:
-      Software Engineering and minoring in Business.
-      <br />
-      <br />
-      I love solving the problem we hate most -- wasting time and money.
-      <br />
-      This passion drives my career, centered on using technology to boost
-      business productivity.
-      <br />
-      I'm addicted to giving my all and am looking forward to combining that passion with my skills in software engineering and business to
-      help you!
-    </p>
-    <img className="banner-pic" src={myBanner} alt="Personal Banner"/>
-    <img className="profile-pic" src={profilePic} alt="Me"/>
-  </div>
+  <Container maxWidth="md" className="content">
+    <Typography variant="h3">Home</Typography>
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
+      <Grid item xs={12} md={8}>
+        <p>
+          Hi, my name is <strong>Brigham Andersen</strong>. I'm a student at
+          Brigham Young University getting a Bachelor's of Science in Computer
+          Science: Software Engineering and minoring in Business.
+        </p>
+        <p>
+          I love solving the problem we hate most --
+          <i> wasting time and money</i>. This passion drives my career,
+          centered on using technology to boost business productivity.
+        </p>
+        <p>
+          I'm addicted to giving my all and am looking forward to combining that
+          passion with my skills in software engineering and business to help
+          you!
+        </p>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <img className="profile" src={profile} alt="Me" />
+      </Grid>
+    </Grid>
+  </Container>
 );
 
 export default Home;
