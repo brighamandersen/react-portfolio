20389f0d2a83746eaf4d765df8499da6a5cb84fc
Brigham Andersen <brighamband@gmail.com>
2021-08-03 18:38:53 -0600

    Updated navbar with proptypes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,30 +1,31 @@
-import { Box, Hidden } from "@material-ui/core";
 import React from "react";
+import PropTypes from "prop-types";
+import { Box, Hidden } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 
-const Navbar = (props) => {
-  const { pages } = props;
-
-  return (
-    <nav className="navbar">
-      {pages.map((page) => (
-        <NavLink
-          key={page.link}
-          className="nav-link"
-          activeClassName="active"
-          exact={page.link === "/"}
-          to={page.link}
-        >
-          <Box display="flex" justifyContent="space-evenly" alignItems="center">
-            <div className="flex-child navlink-icon">{page.icon}</div>
-            <Hidden smDown>
-              <div className="flex-child navlink-text">{page.displayText}</div>
-            </Hidden>
-          </Box>
-        </NavLink>
-      ))}
-    </nav>
-  );
-};
+const Navbar = ({ pages }) => (
+  <nav className="navbar">
+    {pages.map((page) => (
+      <NavLink
+        key={page.path}
+        className="nav-link"
+        activeClassName="active"
+        exact={page.path === "/"}
+        to={page.path}
+      >
+        <Box display="flex" justifyContent="space-evenly" alignItems="center">
+          <div className="flex-child navlink-icon">{page.icon}</div>
+          <Hidden smDown>
+            <div className="flex-child navlink-text">{page.name}</div>
+          </Hidden>
+        </Box>
+      </NavLink>
+    ))}
+  </nav>
+);
 
 export default Navbar;
+
+Navbar.propTypes = {
+  pages: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
