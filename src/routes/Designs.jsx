61c979b0f40301073667ecb753807bf721fed850
Brigham Andersen <brighamband@gmail.com>
2021-08-12 12:04:47 -0500

    Added designs

--- src/routes/Designs.jsx
+++ src/routes/Designs.jsx
@@ -1,35 +1,32 @@
 import React from "react";
-import {  Grid, Container, makeStyles } from "@material-ui/core";
-import { PageTop, /*useGlobalStyles*/ } from "../global";
-import registerShot from '../assets/designs/register.png';
-import recordsShot from '../assets/designs/records.png';
-import notFoundShot from '../assets/designs/not-found.png';
+import { Grid, Container, makeStyles } from "@material-ui/core";
+import { PageTop } from "../global";
+import { designShots } from "../assets/data";
 
 const useLocalStyles = makeStyles(() => ({
   screenshot: {
-    width: '100%',
+    width: "100%",
     boxShadow: "0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)",
-  }
+  },
 }));
 
 const Designs = () => {
-  // const global = useGlobalStyles();
   const local = useLocalStyles();
 
   return (
     <main>
-      <Container maxWidth="lg">
-        <PageTop pageTitle="My Design Screenshots" />
-        <Grid container spacing={3}>
-            <Grid item xs={6} md={4}>
-              <img src={registerShot} alt="App Screenshot" className={local.screenshot} />
+      <Container maxWidth="md">
+        <PageTop pageTitle="My Designs" />
+        <Grid container spacing={5}>
+          {designShots.map((shot) => (
+            <Grid key={shot} item xs={12} sm={6} md={4}>
+              <img
+                src={shot}
+                alt="App Screenshot"
+                className={local.screenshot}
+              />
             </Grid>
-            <Grid item xs={6} md={4}>
-              <img src={recordsShot} alt="App Screenshot" className={local.screenshot} />
-            </Grid>
-            <Grid item xs={6} md={4}>
-              <img src={notFoundShot} alt="App Screenshot" className={local.screenshot} />
-            </Grid>
+          ))}
         </Grid>
       </Container>
     </main>
