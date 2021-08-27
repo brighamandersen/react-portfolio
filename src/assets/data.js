ea41d104d2d8510a26c6248148a9aa8fd9ed66b7
Brigham Andersen <brighamband@gmail.com>
2021-08-27 16:58:45 -0600

    updated data for silver fund

--- src/assets/data.js
+++ src/assets/data.js
@@ -308,7 +308,7 @@ export const sites = [
 //     name: 'Silver Fund Web App',
 //     url: 'https://47fund.byu.edu',
 //     category: 'work',
-//     description: 'Stocks/Trades Performance Predictor/Calculator Site',
+//     description: 'Silver Fund Web App - A Stocks/Trades Performance Tracker/Predictor Site.  I created this while working as a Software Engineer for Silver Fund (BYU's MBA Finance Program) during the second half of 2020.",
 //     srcCode: 'FIXME',
 // },
 
