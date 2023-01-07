b439d2da5b1605aa2fbac2846570f823ccb05e80
Brigham Andersen <brighamband@gmail.com>
2023-01-07 12:30:15 -0700

    added ids to all pics

--- src/data.tsx
+++ src/data.tsx
@@ -290,16 +290,19 @@ export const projects: Project[] = [
 export const allShots = projects.flatMap(project => project.shots);
 
 interface Site {
+  id: string; // Name of repo / gdrive folder and files
   name: string;
   url: string | null;
   srcCode: string | null;
   category: string[];
   description: string;
+  // shots: string[];
 }
 
 /* List of the websites I've made */
 export const sites: Site[] = [
   {
+    id: "commandshake",
     name: "Command Shake",
     url: null,
     srcCode: "https://github.com/brighamband/commandshake",
@@ -308,6 +311,7 @@ export const sites: Site[] = [
       "A script that automatically applies to jobs for me.  Web scrapes the BYU Handshake website when you search for any job and applies to all the jobs with quick apply.  Written in Python.",
   },
   {
+    id: "pecos",
     name: "Pecos Solutions",
     url: "https://pecos-solutions.com",
     srcCode: "https://github.com/Pecos-Solutions/pecos",
@@ -316,6 +320,7 @@ export const sites: Site[] = [
     "My latest commercial website.  A full-stack web app used to preview and download records/documents by county.  Features user authentication, custom theming, an Admin Dashboard, and dynamic routing.  Back-end is AWS, front-end is React, using Styled Components and Material UI for styling.",
   },
   {
+    id: "47fund",
     name: "Silver Fund Web App",
     url: "https://47fund.brighamband.com",
     srcCode: "https://github.com/brighamband/47fund",
@@ -324,6 +329,7 @@ export const sites: Site[] = [
     "Silver Fund Web App - A Stocks/Trades Performance Tracker/Predictor Site.  I created this while working as a Software Engineer for Silver Fund (BYU's MBA Finance Program) during the second half of 2020.  After I left the project broke, so I recreated the front end side of things on my domain as a side project.",
   },
   {
+    id: "vbb-portal",
     name: "VBB Mentoring Portal",
     url: "https://vbb.brighamband.com",
     srcCode: "https://github.com/brighamband/vbb-portal",
@@ -332,6 +338,7 @@ export const sites: Site[] = [
       "International Mentoring Booking Site used by Nike and Oracle.  I did full stack development on this project, but focused primarily on front end.  This project has been unstable since I left, so in the cases that the Official Website (https://portal.villagebookbuilders.org) is down or people want to take a look at site features w/o making an account, I recreated the front end side of things on my domain as a side project.",
   },
   {
+    id: "ontheline",
     name: "On The Line",
     url: "https://ontheline.brighamband.com",
     srcCode: "https://github.com/brighamband/on-the-line",
@@ -340,14 +347,16 @@ export const sites: Site[] = [
       "A scorekeeper game built specifically around the basketball mini game \"9 On the Line\". I made this in React after getting frustrated of keeping track of game scores on my own.",
   },
   {
+    id: "ponderizer",
     name: "Ponderizer",
     url: "https://ponderizer.brighamband.com",
     srcCode: "https://github.com/brighamband/ponderizer",
     category: ["school"],
     description:
-      "Full-stack scripture memorization app - make an account, then save and ponderize your favorite scripture verses! Created with VueJS, Node, Express, and MongoDB.  ",
+      "Full-stack scripture memorization app - make an account, then save and ponderize your favorite scripture verses! Created with VueJS, Node, Express, and MongoDB.  No longer hooked up to the back-end, but feel free to check out the source code!",
   },
   {
+    id: "nba-insider-hub",
     name: "NBA Insider Hub",
     url: "https://nba.brighamband.com",
     srcCode: "https://github.com/brighamband/nba-insider-hub",
@@ -356,6 +365,7 @@ export const sites: Site[] = [
       "4 words: Basketball Player Search Index.  Front-end client which connects the balldontlie API, allowing users to see a table of NBA Teams, then search by player name to see latest player stats. Uses Vanilla JavaScript, HTML, and Bootstrap CSS.",
   },
   {
+    id: "jokester",
     name: "Jokester",
     url: "https://jokester.brighamband.com",
     srcCode: "https://github.com/brighamband/jokester",
@@ -364,6 +374,7 @@ export const sites: Site[] = [
       "A website which displays random coding and dad jokes, then allows you to save your favorites so you can view them later.  Uses Vanilla JavaScript, HTML, and Bulma CSS (decided I'd try a different CS framework on this one).",
   },
   {
+    id: "scoreboard",
     name: "Scoreboard",
     url: "https://scoreboard.brighamband.com",
     srcCode: "https://github.com/brighamband/scoreboard",
@@ -372,6 +383,7 @@ export const sites: Site[] = [
       "Birthday present for my dad (he loves sports and stats, so he naturally loves scorekeeping).  I've made many sites in React, but this was my first website made using Material UI for styling.",
   },
   {
+    id: "svelter",
     name: "Svelter",
     url: "https://svelter.brighamband.com",
     srcCode: "https://github.com/brighamband/svelter",
@@ -380,6 +392,7 @@ export const sites: Site[] = [
       "A simple website I made using Svelte Kit which showcases basic features of Svelte.",
   },
   {
+    id: "simple-counter",
     name: "Simple Counter",
     url: "https://counter.brighamband.com",
     srcCode: "https://github.com/brighamband/simple-counter",
@@ -388,6 +401,7 @@ export const sites: Site[] = [
       "Ever run out of fingers while counting?  Use this app to keep tally! Simple Counter was one of my first React apps back when I was first learning about React Hooks.",
   },
   {
+    id: "simple-calculator",
     name: "Simple Calculator",
     url: "https://calculator.brighamband.com",
     srcCode: "https://github.com/brighamband/simple-calculator",
@@ -396,6 +410,7 @@ export const sites: Site[] = [
       "Simple reactive calculator that takes to inputs and returns the result on the fly after performing basic arithmetic ( + - * / ).  Made with React.",
   },
   {
+    id: "weather-rain-or-shine",
     name: "Weather Rain or Shine",
     url: "https://weather.brighamband.com",
     srcCode: "https://github.com/brighamband/weather-rain-or-shine",
@@ -404,6 +419,7 @@ export const sites: Site[] = [
       "A weather app that lets you search by city, then interacts with an API to display current weather conditions, 5-day, and 3-hour forecasts.  Uses Vanilla JavaScript, HTML, and Bootstrap CSS.",
   },
   {
+    id: "k9-directory",
     name: "K9 Directory",
     url: "https://k9.brighamband.com",
     srcCode: "https://github.com/brighamband/k9-directory",
@@ -412,6 +428,7 @@ export const sites: Site[] = [
       "Consider yourself a dog-lover?  Here's a simple React app made in CodeSandbox which interfaces with an API to show random dog pictures based on the breed you select.",
   },
   {
+    id: "snake",
     name: "Snake",
     url: "https://snake.brighamband.com",
     srcCode: "https://github.com/brighamband/snake",
@@ -420,6 +437,7 @@ export const sites: Site[] = [
       "A blast from the past, this retro snake game will give you some serious nostalgia.  Made using Vanilla JavaScript, HTML, and Standard CSS.",
   },
   {
+    id: "piano",
     name: "Piano",
     url: "https://piano.brighamband.com",
     srcCode: "https://github.com/brighamband/piano",
@@ -428,6 +446,7 @@ export const sites: Site[] = [
       "On the go but dying to pound some horse teeth?  Here's this online mini piano (1 octave) created using Vanilla JavaScript, HTML, and Standard CSS.",
   },
   {
+    id: "tictactoe",
     name: "Tic Tac Toe",
     url: "https://tictactoe.brighamband.com",
     srcCode: "https://github.com/brighamband/tictactoe",
@@ -436,6 +455,7 @@ export const sites: Site[] = [
       "Don't have a pen and paper but one play some good 'ole Tic Tac Toe?  Here you go!  This website was made using Vanilla JavaScript, HTML, and Standard CSS.",
   },
   {
+    id: "photography",
     name: "Photography",
     url: "https://photography.brighamband.com",
     srcCode: "https://github.com/brighamband/photography",
@@ -444,6 +464,7 @@ export const sites: Site[] = [
       "Have an exciting event coming up and want to make sure you capture those memories?  Here's an photographer informational site where you look into hiring a photographer.  Created using HTML and Bootstrap CSS.",
   },
   {
+    id: "mui-todo",
     name: "Material Todo",
     url: "https://mui-todo.brighamband.com",
     srcCode: "https://github.com/brighamband/mui-todo",
@@ -452,6 +473,7 @@ export const sites: Site[] = [
       "For a job interview I made this basic todo list showing my skills in Material UI and React.  This was back when I was less experienced and I was on a time crunch so it still needs advanced functionality (needs to add todos, remove them, and save them for future reference.",
   },
   {
+    id: "silver-fund",
     name: "Silver Fund Visitor Site",
     url: "https://silverfund.byu.edu",
     srcCode: null,
@@ -460,6 +482,7 @@ export const sites: Site[] = [
       "My first no-code website, used making WordPress.  The head of our organization was looking to create an informational page for visitors and wanted to be able to go in later and make adjustments himself, so we opted to make the site in WordPress.",
   },
   {
+    id: "timekeeper",
     name: "Timekeeper",
     url: "https://timekeeper.brighamband.com",
     srcCode: "https://github.com/brighamband/timekeeper",
@@ -467,6 +490,7 @@ export const sites: Site[] = [
     description: "A modern redesign of online-stopwatch.com.  Makes it easy to keep a countdown or stopwatch going in the background of your browser.",
   },
   {
+    id: "no-end-insight",
     name: "No End Insight",
     url: "https://insight.brighamband.com",
     srcCode: "https://github.com/brighamband/no-end-insight",
@@ -475,6 +499,7 @@ export const sites: Site[] = [
       "An online social media platform for sharing uplifting insights!  Still under development, only the front-end (HTML and Bootstrap CSS) was completed for school.  Now I'm working on hooking it up to a back-end for fun!",
   },
   {
+    id: "comics",
     name: "Comics Browser",
     url: "https://comics.brighamband.com",
     srcCode: "https://github.com/brighamband/comics",
@@ -483,6 +508,7 @@ export const sites: Site[] = [
       "Online comics browser.  Look through comics, then leave ratings and comments.  Made possible with an API for comic data, then created with VueJS.",
   },
   {
+    id: "groceries",
     name: "Mother Earth Groceries",
     url: "https://groceries.brighamband.com",
     srcCode: "https://github.com/brighamband/groceries",
@@ -491,6 +517,7 @@ export const sites: Site[] = [
       "Sick of going in-person to the store just to see their selection?  This site showcases a grocery store and their unique product offering, allowing users to search for specific items, then add desired ones to their cart.  Made using VueJS.",
   },
   {
+    id: "photobomb",
     name: "Photo Bomb",
     url: null,
     srcCode: "https://github.com/brighamband/photobomb",
@@ -499,6 +526,7 @@ export const sites: Site[] = [
       "Photos uploader, sharer, and viewer.  Now deprecated since previous hosting subscription expired.",
   },
   {
+    id: "museum",
     name: "Museum of Ordinary Objects",
     url: null,
     srcCode: "https://github.com/brighamband/museum",
@@ -507,20 +535,13 @@ export const sites: Site[] = [
       "Random picture uploader/viewer that introduced me to MongoDB.  Now deprecated since previous hosting subscription expired.",
   },
   {
-    name: "Say Hello Greeter",
-    url: "https://codepen.io/brighamband/pen/qBrxPgg",
-    srcCode: "https://codepen.io/brighamband/pen/qBrxPgg",
-    category: ["side"],
-    description:
-      "A simple app which greets a user based on the name they enter.  I was learning Android and picked this basic project, but decided to first implement it using Vanilla JavaScript to compare and contrast the two.",
-  },
-  {
+    id: "portfolio",
     name: "Portfolio (This Website)",
     url: "https://brighamband.com",
     srcCode: "https://github.com/brighamband/portfolio",
     category: ["side", "work"],
     description:
-      "This website was made using React and styled using Material UI.  It was built with the purpose of helping prospective clients/employers get a feel for who I am and the work I do.",
+      "This website was made using React and styled using Material UI.  It was built with the purpose of helping prospective clients/employers get a feel for who I am and the work I do. Though the site looks pretty simple, I've added a lot of optimizations so that images load quickly and there's no lag across the experience.",
   },
 ];
 
