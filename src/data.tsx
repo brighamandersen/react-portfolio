57e7c00095c2b49086eb8a39477ed08711f8bbc8
Brigham Andersen <brighamband@gmail.com>
2023-07-23 13:33:14 -0600

    Update linkedin link to brighamandersen

--- src/data.tsx
+++ src/data.tsx
@@ -618,7 +618,7 @@ export const contactMethods: ContactMethod[] = [
   {
     name: 'LinkedIn',
     icon: LinkedInIcon,
-    link: 'https://www.linkedin.com/in/brighamband',
+    link: 'https://www.linkedin.com/in/brighamandersen',
     tooltipTitle: 'Connect with me on LinkedIn'
   },
   {
