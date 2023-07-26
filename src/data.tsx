e634c10ef534fe780fc8fe8956fca60f8715a8f8
Brigham Andersen <brighamband@gmail.com>
2023-07-26 13:14:16 -0600

    Add src code for irecognize

--- src/data.tsx
+++ src/data.tsx
@@ -326,7 +326,7 @@ export const projects: Project[] = [
     id: 'irecognize',
     name: 'iRecognize',
     url: 'https://www.figma.com/proto/dLqZF2Wl8mUQlJtrL1eLfV/iRecognize?node-id=26%3A13',
-    srcCode: null,
+    srcCode: 'https://github.com/brighamandersen/irecognize',
     tags: ['school', 'side'],
     description:
       "For BYU's CS 356 UI/UX Design class project, my group of 3 made a mobile app throughout the second half of the semester. We found that it's hard to remember people's names, and sometimes important details about where you two met or other relevant info about them. We made a Figma prototype and then a Flutter app that demos how you could use your GPS to locate those around you and be able to put names to faces.",
